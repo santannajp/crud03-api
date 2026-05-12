23151ef00d4171c43d8b11490d63fcc30f76b644
santannajp <sanannajp@gmail.com>
2026-05-12 19:37:25 -0300

    dissecando documentação

---
+++ main.py
@@ -0,0 +1,17 @@
+from anyio import to_thread
+
+from typing import Union
+from fastapi import FastAPI
+import requests
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+response = requests.get("http://127.0.0.1:9000/")
